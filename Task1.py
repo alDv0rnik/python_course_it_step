f52060b1775f45d362bcbb1377bfa56105c5dce6
AntRylov <antosha_elninobenni@tut.by>
2023-05-27 09:38:53 +0300

    New Task files

---
+++ Task1.py
@@ -0,0 +1,8 @@
+#Перевести строку "it_step_courses" из shake case в camel case
+
+
+a = "it_step_courses"
+b = a.replace("_", '').replace("i","I",1).replace("s","S",1).replace("c","C",1)
+print(b)
+
+
---
+++ Task2.py
@@ -0,0 +1,13 @@
+a = input()
+b = a.count('0') + \
+    a.count('1') + \
+    a.count('2') + \
+    a.count('3') + \
+    a.count('4') + \
+    a.count('5') + \
+    a.count('6') + \
+    a.count('7') + \
+    a.count('8') + \
+    a.count('9')
+print(b)
+   
